8ecb1f9ac9adab1f3f9f4b20905b63b4a154efc6
Rayhan <rayhankhan1266@gmail.com>
2025-08-14 22:02:44 +0600

    updated URL endpoint

--- mainserver/myapp/urls.py
+++ mainserver/myapp/urls.py
@@ -1,11 +1,12 @@
-from django.conf import settings
-from django.conf.urls.static import static
-from .views import YOLOSegmentAnalyzeView
 from django.urls import path
+from .views import analyze_upload
 
 urlpatterns = [
-    path("detect-analyze/", YOLOSegmentAnalyzeView.as_view(), name="yolo-segment-analyze"),
+    path('analyze/', analyze_upload, name='analyze_upload'),
+    path('detect-analyze/', analyze_upload, name='analyze_upload_alias'),
 ]
 
-# if settings.DEBUG:
-#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# # if settings.DEBUG:
+# #     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
